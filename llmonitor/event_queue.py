b03b22bcaf5257b6ff69a402501e3c9fef4f95b9
vincelwt <vince@lyser.io>
2023-09-06 11:48:09 +0200

    debounce

--- llmonitor/event_queue.py
+++ llmonitor/event_queue.py
@@ -1,6 +1,49 @@
 import threading
 import requests
 
+from threading import Timer
+from inspect import signature
+import time
+
+
+def debounce(wait):
+    def decorator(fn):
+        sig = signature(fn)
+        caller = {}
+
+        def debounced(*args, **kwargs):
+            nonlocal caller
+
+            try:
+                bound_args = sig.bind(*args, **kwargs)
+                bound_args.apply_defaults()
+                called_args = fn.__name__ + str(dict(bound_args.arguments))
+            except:
+                called_args = ''
+
+            t_ = time.time()
+
+            def call_it(key):
+                try:
+                    # always remove on call
+                    caller.pop(key)
+                except:
+                    pass
+
+                fn(*args, **kwargs)
+
+            try:
+                # Always try to cancel timer
+                caller[called_args].cancel()
+            except:
+                pass
+
+            caller[called_args] = Timer(wait, call_it, [called_args])
+            caller[called_args].start()
+
+        return debounced
+
+    return decorator
 
 class EventQueue:
     def __init__(self, api_url, interval=0.5):
@@ -8,16 +51,20 @@ class EventQueue:
         self.interval = interval
         self.api_url = api_url
         self.lock = threading.Lock()
-        self.start_timer()
+    #     self.start_timer()
 
     def add_event(self, event):
+        print('add_event', event)
         with self.lock:
             self.queue.append(event)
+            self.send_events()
 
+    @debounce(0.5)
     def send_events(self):
         with self.lock:
             batch = self.queue
             self.queue = []
+
 
         if batch:
             try:
@@ -32,7 +79,7 @@ class EventQueue:
             except Exception as e:
                 self.add_event(batch)
 
-        self.start_timer()
+        # self.start_timer()
 
-    def start_timer(self):
-        threading.Timer(self.interval, self.send_events).start()
+    # def start_timer(self):
+    #     threading.Timer(self.interval, self.send_events).start()
---
+++ tests/monitor-openai.py
@@ -0,0 +1,11 @@
+import openai
+
+from llmonitor import monitor
+
+monitor(openai)
+
+# create a chat completion
+chat_completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": "Hello world"}])
+
+# print the chat completion
+print(chat_completion.choices[0].message.content)
