bd8f2ec9584d4673591b62904ed3722ad4b7b7ff
hughcrt <chocart.hugues@icloud.com>
2023-10-11 13:44:29 +0200

    Add identify in exports

--- llmonitor/__init__.py
+++ llmonitor/__init__.py
@@ -10,7 +10,7 @@ from .parsers import (
 from .openai_utils import OpenAIUtils
 from .event_queue import EventQueue
 from .consumer import Consumer
-from .users import user_ctx, user_props_ctx
+from .users import user_ctx, user_props_ctx, identify
 
 run_ctx = ContextVar("run_ids", default=None)
 
