107aa31fc2a0b9f09466b95f5cd83dd2a3c8ee91
hughcrt <chocart.hugues@icloud.com>
2023-11-21 14:06:57 +0000

    v0.0.41

--- llmonitor/tags.py
+++ llmonitor/tags.py
@@ -1,16 +1,16 @@
-
 from contextvars import ContextVar
 
-tags_ctx = ContextVar("user_ctx", default=None)
+tags_ctx = ContextVar("tag_ctx", default=None)
+
 
 class TagsContextManager:
     def __init__(self, tags: [str]):
         tags_ctx.set(tags)
 
-    def __enter__(self): 
+    def __enter__(self):
         pass
 
-    def __exit__(self, exc_type, exc_value, exc_tb): 
+    def __exit__(self, exc_type, exc_value, exc_tb):
         tags_ctx.set(None)
 
 
