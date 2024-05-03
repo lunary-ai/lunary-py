b49837b1c5bb1556e99ed85c8d921dc29bc6be23
hughcrt <chocart.hugues@icloud.com>
2024-05-02 21:41:15 -0300

    chore: update azure example

--- examples/azure.py
+++ examples/azure.py
@@ -1,34 +1,27 @@
 import os
-import requests
-import json
-import openai
-
-from dotenv import load_dotenv
-
-load_dotenv()
-
+from openai import AzureOpenAI
 import lunary
 
-openai.api_key = os.environ.get("AZURE_OPENAI_KEY")
-openai.api_base = os.environ.get(
-    "AZURE_OPENAI_ENDPOINT"
-)  # your endpoint should look like the following https://YOUR_RESOURCE_NAME.openai.azure.com/
-openai.api_type = "azure"
-openai.api_version = "2023-05-15"  # this may change in the future
+DEPLOYMENT_ID = os.environ.get("AZURE_OPENAI_DEPLOYMENT_ID")
+RESOURCE_NAME = os.environ.get("AZURE_OPENAI_RESOURCE_NAME")
+API_KEY = os.environ.get("AZURE_OPENAI_API_KEY")
 
-lunary.monitor(openai)
 
-# print(os.environ.get("AZURE_OPENAI_KEY"))
-# print(os.environ.get("AZURE_OPENAI_ENDPOINT"))
-
-deployment_name = "Sonder16kTurbo"  # This will correspond to the custom name you chose for your deployment when you deployed a model.
-# model='gpt-3.5-turbo' #This will correspond to the custom name you chose for your deployment when you deployed a model.
-
-# Send a completion call to generate an answer
-print("Sending a test completion job")
-
-completion = openai.ChatCompletion.create(
-    engine=deployment_name, messages=[{"role": "user", "content": "Hello world"}]
+client = AzureOpenAI(
+    api_version="2023-07-01-preview",
+    api_key=API_KEY,
+    azure_endpoint=f"https://{DEPLOYMENT_ID}.openai.azure.com",
 )
 
-print(completion.choices[0].message.content)
+lunary.monitor(client)
+
+completion = client.chat.completions.create(
+    model=RESOURCE_NAME,
+    messages=[
+        {
+            "role": "user",
+            "content": "How do I output all files in a directory using Python?",
+        },
+    ],
+)
+print(completion.to_json())
