d9a81b994e59cdde554e22191077699272335df4
Thraize <gamerxville@gmail.com>
2024-07-07 21:38:56 +0000

    fix: Fixed support for using metadatas with anthropic clients
    Signed-off-by: Thraize <gamerxville@gmail.com>

--- examples/anthrophic.py
+++ examples/anthrophic.py
@@ -1,4 +1,3 @@
-
 import os
 import asyncio
 from dotenv import load_dotenv
@@ -8,50 +7,45 @@ from lunary.anthrophic import monitor
 
 load_dotenv()
 
-def sync_non_streaming():
+
+def test_sync_non_streaming():
     client = Anthropic()
     monitor(client)
 
     message = client.messages.create(
         max_tokens=1024,
-        messages=[
-            {
-                "role": "user",
-                "content": "Hello, Claude",
-            }
-        ],
+        messages=[{
+            "role": "user",
+            "content": "Hello, Claude",
+        }],
         model="claude-3-opus-20240229",
     )
     print(message.content)
 
 
-async def async_non_streaming():
+async def test_async_non_streaming():
     client = monitor(AsyncAnthropic())
 
     message = await client.messages.create(
         max_tokens=1024,
-        messages=[
-            {
-                "role": "user",
-                "content": "Hello, Claude",
-            }
-        ],
+        messages=[{
+            "role": "user",
+            "content": "Hello, Claude",
+        }],
         model="claude-3-opus-20240229",
     )
     print(message.content)
 
 
-def sync_streaming():
+def test_sync_streaming():
     client = monitor(Anthropic())
 
     stream = client.messages.create(
         max_tokens=1024,
-        messages=[
-            {
-                "role": "user",
-                "content": "Hello, Claude",
-            }
-        ],
+        messages=[{
+            "role": "user",
+            "content": "Hello, Claude",
+        }],
         model="claude-3-opus-20240229",
         stream=True,
     )
@@ -59,19 +53,15 @@ def sync_streaming():
         print(event)
 
 
-        
-
-async def async_streaming():
+async def test_async_streaming():
     client = monitor(AsyncAnthropic())
 
     stream = await client.messages.create(
         max_tokens=1024,
-        messages=[
-            {
-                "role": "user",
-                "content": "Hello, Claude",
-            }
-        ],
+        messages=[{
+            "role": "user",
+            "content": "Hello, Claude",
+        }],
         model="claude-3-opus-20240229",
         stream=True,
     )
@@ -79,9 +69,35 @@ async def async_streaming():
         print(event)
 
 
-# sync_non_streaming()
-# asyncio.run(async_non_streaming())
+def test_extra_arguments():
+    client = Anthropic()
+    monitor(client)
 
-# sync_streaming()
-asyncio.run(async_streaming())
+    message = client.messages.create(
+        max_tokens=1024,
+        messages=[{
+            "role": "user",
+            "content": "Hello, Claude",
+        }],
+        model="claude-3-opus-20240229",
+        tags=["translate"],
+        user_id="user123",
+        user_props={
+            "name": "John Doe",
+        },
+        metadata={
+            "test": "hello",
+            "isTest": True,
+            "testAmount": 123,
+        },
+    )
+    print(message.content)
 
+
+# test_sync_non_streaming()
+# test_asyncio.run(async_non_streaming())
+
+# test_sync_streaming()
+# test_asyncio.run(async_streaming())
+
+test_extra_arguments()
